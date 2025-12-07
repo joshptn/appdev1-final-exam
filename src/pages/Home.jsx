0623311e0ee7f8374c4fe5a82d01769178ced693
auhsojp <jashuapatenia15@gmail.com>
2025-12-07 19:51:31 +0800

    Implement Home component

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import { useNavigate } from "react-router-dom"
+
+function Home () {
+  const navigate = useNavigate()
+
+  return (
+    <div>
+      <h1>Welcome to the Todo App</h1>
+      <button onClick={() => navigate("/login")}>Go to Login</button>
+    </div>
+  )
+}
+
+export default Home
